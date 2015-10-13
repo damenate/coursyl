c95fba75d1e16642c575b9789d0b347aa95055b4
Da-Me <damekim1206@gmail.com>
2015-10-13 15:58:11 -0400

    add JQuery to update JS

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,40 +15,41 @@
 //= require d3
 //= require_tree .
 function updateCourse() {
-  var el = document.getElementsByClassName("btn")[6];
-  el.disabled = true;
+  $(".btn").last().attr("disabled", true)
+  $(".edit_course").submit()
 }
 
-function showHideRow() {
-  var invisLast = document.getElementsByClassName("row")
-  invisLast[invisLast.length-1].style.display = 'none';
+$(function (){
+  $(".btn").last().on("click", updateCourse)
+});
+
+function hideLastRow() {
+  last = $(".association.container").last()
+  if(last) {
+    last.hide();
+  }
 }
+
+$(hideLastRow);
 
 function showLastRow() {
-  var invisLast = document.getElementsByClassName("row")
-  invisLast[invisLast.length-1].style.display = 'block';
+  $(".association.container").last().show();
 }
 
-function showDeleteRow() {
-  var killRow = document.getElementsByClassName("row")
-  killRow.style.display = 'none';
-}
+$(function (){
+  $(".new-association").on("click", showLastRow);
+});
 
 function hideDeleteRow(){
-  var hideRow = event.target.parentElement.parentElement.parentElement;
-  var markDestroy = event.target.parentElement.lastElementChild;
-  hideRow.style.display = "none";
-  markDestroy.checked = true;
+  $(event.target).closest(".association.container").hide();
+  $(event.target).siblings().last().prop("checked", true);
 }
-// function hideDeleteRow() {
-//   var elhide = document.getElementsByClassName("row");
-//   elhide.style.display = 'none';
-//   document.getElementsByClassName("destroy").checked = true
-// // }
+function clickAndDelete() {
+  $(".btn-danger").on("click", hideDeleteRow);
+}
 
+$(clickAndDelete);
 
-//****************************
-//working JQuery
 $(function() {
   $('a[href*=#]:not([href=#])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
@@ -63,138 +64,3 @@ $(function() {
     }
   });
 });
-//****************************
-
-
-/////////////////////////////////////////////////////
-// danai's code (doesnt work)
-// (function smoothscroll() {
-//
-// // Scroll Variables (tweakable)
-// var defaultOptions = {
-//
-//     // Scrolling Core
-//     frameRate        : 150, // [Hz]
-//     animationTime    : 400, // [px]
-//     stepSize         : 120, // [px]
-//
-//     // Pulse (less tweakable)
-//     // ratio of "tail" to "acceleration"
-//     pulseAlgorithm   : true,
-//     pulseScale       : 4,
-//     pulseNormalize   : 1,
-//
-//     // Acceleration
-//     accelerationDelta : 20,  // 20
-//     accelerationMax   : 1,   // 1
-//
-//     // Keyboard Settings
-//     keyboardSupport   : true,  // option
-//     arrowScroll       : 50,     // [px]
-//
-//     // Other
-//     touchpadSupport   : true,
-//     fixedBackground   : true,
-//     excluded          : ''
-// };
-//
-// var options = defaultOptions;
-//
-//
-// // Other Variables
-// var isExcluded = false;
-// var isFrame = false;
-// var direction = { x: 0, y: 0 };
-// var initDone  = false;
-// var root = document.documentElement;
-// var activeElement;
-// var observer;
-// var deltaBuffer = [];
-// var isMac = /^Mac/.test(navigator.platform);
-//
-// var key = { left: 37, up: 38, right: 39, down: 40, spacebar: 32,
-//             pageup: 33, pagedown: 34, end: 35, home: 36 };
-/////////////////////////////////////////////////////
-
-
-/////////////////////////////////////////////////////
-// cruzenunez's code (doesnt work)
-// function currentYPosition() {
-//     if (self.pageYOffset) return self.pageYOffset;
-//     return 0;
-// }
-
-// function elmYPosition(id) {
-//     var elm = document.getElementById(id);
-//     var y = elm.offsetTop;
-//     var node = elm;
-//     while (node.offsetParent && node.offsetParent != document.body) {
-//         node = node.offsetParent;
-//         y += node.offsetTop;
-//     } return y;
-// }
-
-// function smoothScroll(id) {
-//     var startY = currentYPosition();
-//     var stopY = elmYPosition(id);
-//     var distance = stopY > startY ? stopY - startY : startY - stopY;
-//     if (distance < 100) {
-//         scrollTo(0, stopY); return;
-//     }
-//     var speed = Math.round(distance / 100);
-//     if (speed >= 20) speed = 20;
-//     var step = Math.round(distance / 25);
-//     var leapY = stopY > startY ? startY + step : startY - step;
-//     var timer = 0;
-//     if (stopY > startY) {
-//         for ( var i=startY; i<stopY; i+=step ) {
-//             setTimeout("window.scrollTo(0, "+leapY+")", timer * speed);
-//             leapY += step; if (leapY > stopY) leapY = stopY; timer++;
-//         } return;
-//     }
-//     for ( var i=startY; i>stopY; i-=step ) {
-//         setTimeout("window.scrollTo(0, "+leapY+")", timer * speed);
-//         leapY -= step; if (leapY < stopY) leapY = stopY; timer++;
-//     }
-//     return false;
-// }
-/////////////////////////////////////////////////////
-
-/////////////////////////////////////////////////////
-//Somewhere online
-// function animate(elem,style,unit,from,to,time,prop) {
-//     if( !elem) return;
-//     var start = new Date().getTime(),
-//         timer = setInterval(function() {
-//             var step = Math.min(1,(new Date().getTime()-start)/time);
-//             if (prop) {
-//                 elem[style] = (from+step*(to-from))+unit;
-//             } else {
-//                 elem.style[style] = (from+step*(to-from))+unit;
-//             }
-//             if( step == 1) clearInterval(timer);
-//         },25);
-//     elem.style[style] = from+unit;
-// }
-//
-// window.onload = function () {
-//     var target = document.getElementById("div5");
-//     animate(document.body, "scrollTop", "", 0, target.offsetTop, 2000, true);
-// };
-/////////////////////////////////////////////////////
-
-
-/////////////////////////////////////////////////////
-//Internet somewhere
-// function scrollTo(element, to, duration) {
-//     if (duration < 0) return;
-//     var difference = to - element.scrollTop;
-//     var perTick = difference / duration * 10;
-//
-//     setTimeout(function() {
-//         element.scrollTop = element.scrollTop + perTick;
-//         if (element.scrollTop === to) return;
-//         scrollTo(element, to, duration - 10);
-//     }, 10);
-// }
-/////////////////////////////////////////////////////
